e2897952890c1d6daf5128fa847f716b08c66b7d
Eric Martindale <eric@ericmartindale.com>
2022-05-10 16:29:05 -0400

    Migrate to ES6 module as export

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+// # Rollup Configuration
+
+// Dependencies
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import minify from 'rollup-plugin-minify'
+import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
 
 export default [{
   input: 'module.js',
-  output: {
-    name: 'FabricReact',
-    file: pkg.main,
-    format: 'umd'
-  },
+  output: [
+    {
+      name: 'FabricReact',
+      file: pkg.main,
+      format: 'es'
+    }
+  ],
   plugins: [
     resolve(),
     babel({ 
