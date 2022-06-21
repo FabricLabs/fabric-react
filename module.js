f33bf16a6d971771d1562c8843fda07f32ee785b
victorwu <victorwu@users.noreply.github.com>
2022-06-20 22:05:45 -0400

    refactor: rm module.js

--- module.js
+++
@@ -1,9 +0,0 @@
-import * as FabricBridge from './components/FabricBridge';
-import * as FabricIdentity from './components/FabricIdentity';
-import * as FabricIdentityManager from './components/FabricIdentityManager';
-
-export default {
-  FabricBridge,
-  FabricIdentity,
-  FabricIdentityManager
-};
