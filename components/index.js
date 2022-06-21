11d4ae9e73dd4cf436d36308948df0172b6917ab
victorwu <victorwu@users.noreply.github.com>
2022-06-20 23:14:48 -0400

    build: update exports

--- components/index.js
+++ components/index.js
@@ -3,6 +3,9 @@ const FabricBridge = require('./FabricBridge');
 const FabricIdentity = require('./FabricIdentity');
 const FabricIdentityManager = require('./FabricIdentityManager');
 
-exports.FabricBridge = FabricBridge;
-exports.FabricIdentity = FabricIdentity;
-exports.FabricIdentityManager = FabricIdentityManager;
+export {
+  FabricBridge,
+  FabricIdentity,
+  FabricIdentityManager
+};
+export default FabricIdentity;
