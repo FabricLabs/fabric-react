3edb6c04e409a13e1fbe42653441b18c19387eab
Eric Martindale <eric@ericmartindale.com>
2023-05-06 01:30:18 -0400

    Add JSX, cleanup tests

--- tests/fabric.react.js
+++ tests/fabric.react.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const assert = require('assert');
-const overview = require('../package');
+import assert from 'assert';
+import * as overview from '../package.json' assert { type: 'json' };
 // const FabricBridge = require('../components/FabricBridge');
 
 describe('@fabric/react', function () {
--- types/fabric.js
+++ types/fabric.js
@@ -1,11 +1,35 @@
-import * as Sample from './components/Sample.js';
+'use strict';
+
+// Types
+import Component from './component.js';
+// import SPA from './spa';
+
+// Components
+// import * as Sample from './components/Sample.js';
 // import * as FabricBridge from './components/FabricBridge';
 // import * as FabricIdentity from './components/FabricIdentity';
 // import * as FabricIdentityManager from './components/FabricIdentityManager';
 
-export default {
-  // FabricBridge,
-  // FabricIdentity,
-  // FabricIdentityManager,
-  Sample
-};
+const state = {};
+
+class Fabric {
+  constructor (settings = {}) {
+    this.settings = Object.assign({
+      alias: '@fabric/react'
+    }, settings);
+  }
+
+  static Component () {
+    return Component;
+  }
+
+  static SPA () {
+    return SPA;
+  }
+
+  get state () {
+    return state;
+  }
+}
+
+export default Fabric;
